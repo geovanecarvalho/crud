cb1dbeb9a23c60772ee940c2eb75723752a09e80
Geovane Carvalho <geovaneoliveira.75@hotmail.com>
2020-10-09 19:53:24 -0300

    import minvaluevalidator

--- crudLivros/core/models.py
+++ crudLivros/core/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import  MinValueValidator
 
 
 class Autor(models.Model):
